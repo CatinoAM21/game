56e96d5a481c380bbfc12d4a74847c77409a5a91
Derek Woodard <thedjw191@gmail.com>
2020-01-31 23:26:17 -0600

    Added ghost blocks and aligned them to a grid

--- Assets/Player2.cs
+++ Assets/Player2.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 
 
@@ -8,16 +9,19 @@ using UnityEngine;
 public class Player2 : MonoBehaviour
 {
     public GameObject block;
+    public GameObject blockPreview;
+    private GameObject bP;
     void Start()
     {
-        
+        bP = Instantiate(blockPreview, new Vector3(0, 0, 11), transform.rotation);
     }
     void Update()
     {
+        Vector3 v3 = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        v3.x = (float)Math.Round((double)v3.x/3)*3; 
+        v3.y = (float)Math.Round((double)v3.y/3)*3;
         if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 v3 = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
             Instantiate(block, v3, transform.rotation);
-        }        
+        bP.transform.position = v3;
     }
 }
