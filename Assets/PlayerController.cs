1f121923b952f9e5259301b5651c4b1e1b0d0261
Anthony <catinoam21@uww.edu>
2020-01-31 18:50:58 -0600

    ffsfasdfsa
    player moves?

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpForce;
+
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    void start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+   
+    
+
+
+
+
+}
