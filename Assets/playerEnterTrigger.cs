eeab3ab24474a1132b108cea814adaa1c75be95c
Anthony <catinoam21@uww.edu>
2020-02-01 20:23:07 -0600

    LEVLE

---
+++ Assets/playerEnterTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerEnterTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
